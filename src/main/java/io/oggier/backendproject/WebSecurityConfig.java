5d48ccd69ae6be3b71cce16102fa835ed3e9828b
camogg <camille@oggier.io>
2019-12-10 01:47:34 +0200

    Security implementation

--- src/main/java/io/oggier/backendproject/WebSecurityConfig.java
+++ src/main/java/io/oggier/backendproject/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package io.oggier.backendproject;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -8,18 +10,28 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    // Creating two user for the demo
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("student").password("{noop}student").roles("STUDENT")
+                .and()
+                .withUser("teacher").password("{noop}teacher").roles("STUDENT", "TEACHER");
+
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
             .authorizeRequests()
-            .antMatchers("/", "/home", "/students", "/students/**", "/teachers", "/teachers/*", "/courses", "/courses/**", "/enrollments", "/enrollments/**").permitAll()
+                .antMatchers(HttpMethod.GET, "/").permitAll()
+                .antMatchers(HttpMethod.GET, "/**").hasAnyRole("STUDENT", "TEACHER")
+                .antMatchers(HttpMethod.POST, "/**").hasRole("TEACHER")
+                .antMatchers(HttpMethod.PUT, "/**").hasRole("TEACHER")
+                .antMatchers(HttpMethod.DELETE, "/**").hasRole("TEACHER")
             .anyRequest().authenticated()
             .and()
-            .formLogin()
-            .loginPage("/login")
-            .permitAll()
-            .and()
-            .logout()
-            .permitAll();
+            .httpBasic();
     }
 }
